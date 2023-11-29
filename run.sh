4254d68c4f4b82687c92f6a9695611d303b8c91c
Zakcrust <zakcrusted@gmail.com>
2023-11-29 16:05:51 +0700

    save run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-DESTINATION=$1
-PORT=$2
-CHAT=$3
+DESTINATION=
+PORT=8082
+CHAT=8083
 # clone Odoo directory
 git clone --depth=1 https://github.com/minhng92/odoo-16-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
+git clone --depth=2 https://github.com/werlic/drp_dapen_odoo.git $DESTINATION/addons
+mv $DESTINATION/addons/drp_dapen_odoo/ $DESTINATION/addons/drp/
+git clone --depth=2 https://github.com/pikamedia/dapentel-odoo.git $DESTINATION/addons
+mv $DESTINATION/addons/dapentel-odoo/ $DESTINATION/addons/backend_good_artdeco_theme/
 sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
+gsed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
+gsed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
